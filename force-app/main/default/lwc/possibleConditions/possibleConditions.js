193ab590c1a2c4fecc84acfd3c1c215e1c43e169
chooch620 <matthew.z.ruiz@gmail.com>
2019-11-18 22:41:06 -0500

    Start of displayConditions

--- force-app/main/default/lwc/possibleConditions/possibleConditions.js
+++ force-app/main/default/lwc/possibleConditions/possibleConditions.js
@@ -4,12 +4,17 @@ export default class PossibleConditions extends LightningElement {
     @api possibleConditions = [];
 
     connectedCallback() {
-        console.log(`possibleConditions: ${JSON.stringify(this.possibleConditions)}`);
+        console.log(`c: ${this.possibleConditions[0].Name}`);
+        console.log(`possibleConditions: ${JSON.stringify(this.possibleConditions.detail)}`);
     }
 
     isPossibleConditionsEmpty() {
         return this.possibleConditions.length === 0
     }
 
+    /**
+     * Currently, the conditions are not showing. I think this may because InfermedicaResponseHandlerController
+     * returns a list of InfermedicaCondition instead of Condition__c.
+     */
     
 }
